587cde2c711dbf56e410161083bb4ce5d0f83682
ctbinh <binh17122001@gmail.com>
2023-09-01 21:04:58 +0700

    api change visibility

--- plugin.rb
+++ plugin.rb
@@ -10,11 +10,13 @@ register_asset 'stylesheets/common.scss'
 
 after_initialize do
     load File.expand_path('../app/controllers/discourse_private_topic_controller.rb', __FILE__)
+    load File.expand_path('../app/controllers/topics_controller.rb', __FILE__)
 
     Discourse::Application.routes.append do
         # Map the path `/name` to `DiscoursePPTController`’s `index` method
         # Remove route if not in use
         # get '/name' => 'discourse_private_topic#index'
+        put "t/:topic_id/change_visibility" => "topics#change_visibility", :constraints => { topic_id: /\d+/ }, defaults: { format: 'json' }
     end
 
     # hide topics from search results
